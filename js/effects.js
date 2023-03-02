e9950af23a2e6d33ef998f68291824d5f8d1cfaf
Herb <herbrholder@gmail.com>
2023-03-01 21:32:09 -0600

    Add: login.html

---
+++ js/effects.js
@@ -0,0 +1,30 @@
+(function(){
+    "use strict"
+    let form = document.getElementById("form");
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+    })
+    const observer = new IntersectionObserver((entries)=>{
+        entries.forEach(entry => {
+            if (entry.isIntersecting){
+                entry.target.classList.add("show");
+            }
+            // else{
+            //     entry.target.classList.remove("show");
+            // }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach(el => {
+        observer.observe(el);
+    })
+
+
+
+
+
+
+
+})();
+
